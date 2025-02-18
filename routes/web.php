20b2ce2a1dea354313fea84b37b1508693aaa560
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2025-02-18 01:54:46 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ require __DIR__ . '/auth.php';
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', fn() => auth()->user())->name('user');
+    Route::get('/uploads', [UploadController::class, 'index']);
     Route::post('/upload', [UploadController::class, 'store'])->name('upload');
     Route::post('/upload/{identifier}/pause', [UploadController::class, 'pause'])->name('upload.pause');
     Route::delete('/upload/{identifier}', [UploadController::class, 'delete'])->name('upload.delete');
-    Route::get('/uploads', [UploadController::class, 'index']);
-
-    Route::get('/storage/{path}', StorageController::class)->where('path', '.*')->name('storage.path');
-
     Route::get('/files', [FileController::class, 'index']);
     Route::delete('/file/{id}', [FileController::class, 'delete']);
+    Route::get('/storage/{path}', StorageController::class)->where('path', '.*')->name('storage.path');
 });
